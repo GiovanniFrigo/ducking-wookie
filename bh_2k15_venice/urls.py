d3eb5d18c993ed21497249b6e32f2f4a14ca6001
Giovanni Frigo <john.frigo92@gmail.com>
2015-07-12 00:07:35 +0200

    I break stuff, i fix stuff

--- bh_2k15_venice/urls.py
+++ bh_2k15_venice/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from core import views
-import payment_gateway
+from payment_gateway import views as payment_views
 
 urlpatterns = [
     url(r'^protoapi/menu/(?P<place>\w+)/$', views.menu_place),
     url(r'^protoapi/menu/(?P<place>\w+)/(?P<year>\w+)/(?P<month>\w+)/$', views.menu_place_booked),
-    url(r'^protoapi/payment/token/$', payment_gateway.views.generate_token),
-    url(r'^protoapi/payment/pay/$', payment_gateway.views.post_payment),
+    url(r'^protoapi/payment/token/$', payment_views.views.generate_token),
+    url(r'^protoapi/payment/pay/$', payment_views.views.post_payment),
     url(r'^admin/', include(admin.site.urls)),
 ]
