c72a49e5e35dda37903eaa470d25c584f51ecb21
Giovanni Frigo <john.frigo92@gmail.com>
2015-07-11 21:19:02 +0200

    Adding admin objects mapping

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from core.models import User
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email')
+admin.site.register(User, UserAdmin)
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'date', 'place')
+admin.site.register(Booking, BookingAdmin)
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+admin.site.register(Menu, MenuAdmin)
+
+class CookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+admin.site.register(Cook, CookAdmin)
--- core/models.py
+++ core/models.py
@@ -67,7 +67,6 @@ class User(AbstractBaseUser, PermissionsMixin):
             self.username = self.email
         super(User, self).save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
 
-
 class Booking(models.Model):
     user = models.ForeignKey(User)
 
