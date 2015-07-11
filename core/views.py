4d360f9af7b33ba3d98eff0875b60ea00bd0bf18
Matteo Scotuzzi <matteo.scotuzzi@gmail.com>
2015-07-11 22:26:39 +0200

    first views

--- core/views.py
+++ core/views.py
@@ -1,9 +1,9 @@
-from django.http.response import HttpResponse, HttpResponseNotFound
+import datetime
+from django.http.response import Http404
 from django.shortcuts import render
 import json
 from django.http import JsonResponse
-from models import Booking
-from models import Menu
+from models import Booking, Menu, Place
 
 
 def menu_place(request, place):
@@ -42,20 +42,48 @@ def menu_place_booked(request, place, year, month):
         'dec':12
     }
     if month not in months:
-        raise HttpResponseNotFound()
+        raise Http404()
 
     month_num = months_dict[month]
+    month_selected = datetime.datetime(year=int(year), month=month_num, day=1)
+    def add_months(sourcedate, months):
+        import calendar
+        month = sourcedate.month - 1 + months
+        year = sourcedate.year + month / 12
+        month = month % 12 + 1
+        day = min(sourcedate.day,calendar.monthrange(year,month)[1])
+        return datetime.datetime(year=year,month=month,day=1) # hardcoded 1 instead of `day` because I need first day of this month
+    end_month = add_months(month_selected, 1) + datetime.timedelta(days=-1)
     data = {}
+    data['menus'] = []
     # menu_set = Menu.objects.filter(school__place=place)
     place_obj = Place.objects.get(name=place)
-    Menu.objects.filter(place=place_obj)
-    closed_spots = Booking.objects.filter(menu__place=place, date__in=[])
-    for booked in closed_spots:
-        if booked.month in data:
-            if booked
-                data[booked.month]
+    menus_in_place = Menu.objects.filter(available_in=place_obj)
+    for menu in menus_in_place:
+        closed_spots = Booking.objects.filter(
+            place=place_obj,
+            menu=menu,
+            date__lt=end_month,
+            date__gt=month_selected
+        ).values(
+            'id',
+            'date'
+        )
+        data['menus'].append({
+            'id': menu.id,
+            'name': menu.name,
+            'description': menu.description,
+            'closedSpots': closed_spots
+        })
 
-    # just return a JsonResponse
     return JsonResponse(data)
+
+
+
+
+
+
+
+
 
 
