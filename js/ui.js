b04bcfadb063b9b729f91a4cb805de20e7c4d16b
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2015-07-11 19:22:15 +0200

    ui mvc

---
+++ js/ui.js
@@ -0,0 +1,22 @@
+/*
+*   Handle UI views dynamic loading on screen  
+*/
+function load_frame_callback(page, container_id, on_complete) {
+    $.get(page, function (data){
+          $(container_id).html(data).ready(on_complete());
+    });
+}
+
+function load_frame(page, container_id) {
+    $.get(page, function (data){
+          $(container_id).html(data);
+    });
+}
+
+function replaceScreen(screenName, on_complete) {
+    if (arguments.length == 1)
+        load_frame( "screens/" + screenName + ".html", "#pageContent" );
+    else if (arguments.length == 2)
+        load_frame_callback( "screens/" + screenName + ".html", "#pageContent", on_complete );
+}
+
