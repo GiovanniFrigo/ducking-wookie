a12e46bf79158e5d949d93e6b52ad6b310acc8dc
Juleship <michelon.giulio@gmail.com>
2015-07-11 18:44:19 +0200

    livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,6 +29,9 @@ module.exports = function(grunt) {
             } 
         },
         watch: {
+            options: {
+                livereload: true,
+            },
             scripts: {
                 files: ['js/*.js'],
                 tasks: ['concat', 'uglify'],
