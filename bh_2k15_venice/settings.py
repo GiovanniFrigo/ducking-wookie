e4bda07548dba23a28733abdebedd54345c575d6
Giovanni Frigo <john.frigo92@gmail.com>
2015-07-12 00:04:32 +0200

    Adding payment gateway

--- bh_2k15_venice/settings.py
+++ bh_2k15_venice/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    # Custom
+    'core',
+    'payment_gateway'
 )
 
 MIDDLEWARE_CLASSES = (
--- bh_2k15_venice/urls.py
+++ bh_2k15_venice/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from core import views
+import payment_gateway
 
 urlpatterns = [
     url(r'^protoapi/menu/(?P<place>\w+)/$', views.menu_place),
     url(r'^protoapi/menu/(?P<place>\w+)/(?P<year>\w+)/(?P<month>\w+)/$', views.menu_place_booked),
+    url(r'^protoapi/payment/token/$', payment_gateway.views.generate_token),
+    url(r'^protoapi/payment/pay/$', payment_gateway.views.post_payment),
     url(r'^admin/', include(admin.site.urls)),
 ]
