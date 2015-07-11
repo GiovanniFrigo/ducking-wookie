56b722c74a18aa25bc4687fafd759e5caf77adf8
Giovanni Frigo <john.frigo92@gmail.com>
2015-07-11 18:03:01 +0200

    Initial project setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bh_2k15_venice.wsgi --log-file -
---
+++ bh_2k15_venice/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for bh_2k15_venice project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bh_2k15_venice.settings")
+
+application = Cling(get_wsgi_application())
