e65b4283632d4e40908a53ae859aadb5f4fbca3d
Giovanni Frigo <john.frigo92@gmail.com>
2015-07-12 11:28:13 +0200

    Static

--- bh_2k15_venice/urls.py
+++ bh_2k15_venice/urls.py
@@ -38,4 +38,7 @@ if settings.DEBUG:
     urlpatterns += patterns('',
         (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': settings.MEDIA_ROOT}))
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT}))
 
--- bh_2k15_venice/wsgi.py
+++ bh_2k15_venice/wsgi.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bh_2k15_venice.settings")
 
-application = Cling(get_wsgi_application())
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
