055a7b13542c25489c0e7e8b75d07d5929c00f45
Matteo Scotuzzi <matteo.scotuzzi@gmail.com>
2015-07-12 08:33:39 +0200

    booking creation in pay view

--- payment_gateway/views.py
+++ payment_gateway/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 import braintree
 from django.http.response import HttpResponseBadRequest, JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from core.models import Booking
+from core.models import Booking, Place, Menu
+import datetime
+from core.views import get_availability_count
 
 
 @csrf_exempt
@@ -19,15 +22,32 @@ def post_payment(request):
         return HttpResponseBadRequest()
 
     nonce = request.POST.get("payment_method_nonce")
-    booking = Booking.objects.get(id=int(request.POST.get("booking_id")))
+
+    try:
+        # received_json_data=json.loads(request.body)
+        place = Place.objects.get(name__iexact=request.POST.get("place"))
+        user, created = get_user_model().objects.get_or_create(email=request.POST.get('email'), username=request.POST.get('email'))
+        menu = Menu.objects.get(id=int(request.POST.get('menu_id')))
+        n_people = int(request.POST.get('n_people'))
+        date_meal = datetime.datetime(request.POST.get('year'), request.POST.get('month'), request.POST.get('day'))
+        if get_availability_count(date_meal, menu, place) > 0:
+            booking = Booking.objects.create(user=user, place=place, guest_number=n_people, menu=menu, date=date_meal)
+        else:
+            return HttpResponseBadRequest("no booking availability here...")
+    except KeyError as ke:
+        return HttpResponseBadRequest(ke)
+    except Exception as ex:
+        return HttpResponseBadRequest(ex)
+
+    created_booking = Booking.objects.get(id=booking.id)
     result = braintree.Transaction.sale({
-        "amount": "%s.00" % booking.menu.price_per_person*booking.guest_number,  #"10.00",
+        "amount": "%s.00" % created_booking.menu.price_per_person*created_booking.guest_number,  #"10.00",
         "payment_method_nonce": nonce
     })
 
     if result.is_success:
-        booking.transaction_id = result.transaction.id
-        booking.save()
+        created_booking.transaction_id = result.transaction.id
+        created_booking.save()
         return JsonResponse({"status": "success!"})
     elif result.transaction:
         return HttpResponse("\nError processing transaction:" +
